9c927ec4caf8f6cbc6799b26cd577856c200f2f6
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-11-15 21:52:24 +0800

    add toast

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/auth";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { Layout } from "./components/Layout";
@@ -28,6 +29,7 @@ function App() {
           </Layout>
         </AuthProvider>
       </BrowserRouter>
+      <Toaster />
     </QueryClientProvider>
   );
 }
