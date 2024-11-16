849a5b3763ea89441f1ce9b3887fe269cd21f7a0
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-11-16 21:45:04 +0800

    add more types

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -3,3 +3,14 @@ export interface User {
   email: string;
   name: string;
 }
+
+export interface Bookmark {
+  id: string;
+  title: string;
+  description: string;
+  url: string;
+  favicon?: string;
+  tags: string[];
+  created_at: string;
+  user_id: string;
+}
